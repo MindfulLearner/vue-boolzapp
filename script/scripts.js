343050f00901bb41e4532d372bad32b950068207
MindfulLearner <dimajosh1009@gmail.com>
2024-09-05 20:31:10 +0200

    aggiunta logica risposta

--- script/scripts.js
+++ script/scripts.js
@@ -11,15 +11,25 @@ createApp (
                 attivo: "",
                 indiceAttivo: 0,
                 messaggioMostrato: '',
+                messaggio: '',
+
+                tempoRisposta: 0,
+                swagTempo: 0,
             }
         },
         methods: {
             selezionato(i) {
                 this.indiceAttivo = i;
                 console.log(this.indiceAttivo);
-
-
-
+            },
+            mandaMessaggio() {
+                this.cards[this.indiceAttivo].messages.push({date: '10/02/2024 12:33:22', message: this.messaggio, status: 'sent'});
+                this.messaggio =  '';
+                this.swagTempo = setInterval(() => {
+                    this.tempoRisposta++;
+                }, 2000);
+                clearInterval(this.swagTempo);
+                this.cards[this.indiceAttivo].messages.push({date: '10/02/2024 15:22:01', message: 'okay', status: 'receive'});
 
             }
         } 
