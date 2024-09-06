fe0d36ec62966b8ff35407d25fce23f1eb2cf691
MindfulLearner <dimajosh1009@gmail.com>
2024-09-06 11:31:51 +0200

    filtraggio sistemato

--- script/scripts.js
+++ script/scripts.js
@@ -19,16 +19,20 @@ createApp (
 
                 ricerca: '',
 
+                cardActive: 0,
+                indexVecchio: 0,
+
 
             }
         },
         methods: {
-            /* sisstemare selezione*/
             selezionato(i) {
-                this.indiceAttivo = i;
+                this.cardActive = this.swagBarraRicerca[i];
+                this.indexVecchio = this.cards.findIndex(cardSwag => cardSwag.name == this.cardActive.name);
+
+                this.indiceAttivo = this.indexVecchio;
                 console.log(this.indiceAttivo);
             },
-
             mandaMessaggio() {
                 if (this.messaggio.trim()){ 
                     this.cards[this.indiceAttivo].messages.push({date: '10/02/2024 12:33:22', message: this.messaggio, status: 'sent'});
@@ -41,8 +45,8 @@ createApp (
         },
         computed: {
             swagBarraRicerca() {
-                return this.cards.filter(card => {
-                    return card.name.toLowerCase().includes(this.ricerca.toLowerCase());
+                return this.cards.filter(cardSwag => {
+                    return cardSwag.name.toLowerCase().includes(this.ricerca.toLowerCase());
                 });
             },
         }
