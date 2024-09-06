3137f5e20b15adf8cbc983ccf097c62a71caff63
MindfulLearner <dimajosh1009@gmail.com>
2024-09-06 10:44:29 +0200

    aggiunto logica

--- script/scripts.js
+++ script/scripts.js
@@ -8,30 +8,43 @@ createApp (
                 cards: dataUsers,
                 messaggiUtente: dataMessaggiUtente,
                 messaggiAmico: dataMessaggiAmico,
+
+
                 attivo: "",
                 indiceAttivo: 0,
+
+
                 messaggioMostrato: '',
                 messaggio: '',
 
-                tempoRisposta: 0,
-                swagTempo: 0,
+                ricerca: '',
+
+
             }
         },
         methods: {
+            /* sisstemare selezione*/
             selezionato(i) {
                 this.indiceAttivo = i;
                 console.log(this.indiceAttivo);
             },
-            mandaMessaggio() {
-                this.cards[this.indiceAttivo].messages.push({date: '10/02/2024 12:33:22', message: this.messaggio, status: 'sent'});
-                this.messaggio =  '';
-                this.swagTempo = setInterval(() => {
-                    this.tempoRisposta++;
-                }, 2000);
-                clearInterval(this.swagTempo);
-                this.cards[this.indiceAttivo].messages.push({date: '10/02/2024 15:22:01', message: 'okay', status: 'receive'});
 
+            mandaMessaggio() {
+                if (this.messaggio.trim()){ 
+                    this.cards[this.indiceAttivo].messages.push({date: '10/02/2024 12:33:22', message: this.messaggio, status: 'sent'});
+                    this.messaggio =  '';
+                    setTimeout(() => {
+                        this.cards[this.indiceAttivo].messages.push({date: '10/02/2024 15:22:01', message: 'okay', status: 'receive'});
+                    }, 1000);
+                }
             }
-        } 
+        },
+        computed: {
+            swagBarraRicerca() {
+                return this.cards.filter(card => {
+                    return card.name.toLowerCase().includes(this.ricerca.toLowerCase());
+                });
+            },
+        }
     }
 ).mount("#app");
